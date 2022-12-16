731b4bf89c34f565550f7c5f99a0b2f4ebcdff52
HarrisonF18 <109466118+HarrisonF18@users.noreply.github.com>
2022-12-16 18:22:07 -0500

    Created deck, dealer, and player classes. Added welcome message

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,17 @@
 #letsgooo
+
+import random
+
+class Deck:
+   self.deck = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+class Dealer:
+    self.cards = []
+    self. score = 0
+
+class Player:
+    self.name = ""
+    self.cards = []
+    self.score = 0
+
+print("Welcome to the Blackjack table")
