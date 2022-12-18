931e6162f22dafd6848fff3bb3644d064920cd8d
HarrisonF18 <109466118+HarrisonF18@users.noreply.github.com>
2022-12-18 14:21:13 -0500

    Added function to move aces to end of a player cards list and a function to compute score.

--- blackjack.py
+++ blackjack.py
@@ -37,7 +37,7 @@ def set_player_name(player):
 set_player_name(player1)
 
 def draw_card(deck, player):
-    random_deck_index = random.randint(0, (len(deck.cards)))
+    random_deck_index = random.randint(0, (len(deck.cards)-1))
     deck.delt_cards.append(deck.cards[random_deck_index])
     player.cards.append(deck.cards[random_deck_index])
     deck.cards.pop(random_deck_index)
@@ -48,12 +48,28 @@ def deal_cards(deck, dealer, player):
     for i in range(1, 3):
         draw_card(deck, dealer)
 
-    
-# deal_cards(game_deck, dealer1, player1)
+#to move aces to end of hand in order for compute_hand_score function to properly assign a 1 or 10 value to the ace.
+def move_aces_to_end_of_all_players_cards(player, dealer):
+    for person in [player, dealer]:
+        aces_in_hand = []
+        for i in person.cards:
+            if i == "A":
+                aces_in_hand.append(i)
+                person.cards.remove(i)
+        for i in aces_in_hand:
+            person.cards.append(i)
 
-# print(dealer1.cards)
-# print(game_deck.delt_cards)
-# print(player1.cards)
 
-
-
+def compute_hand_score(player):
+    hand_score = 0
+    for i in player.cards:
+        if type(i) == int:
+            hand_score += i
+        if i == "K" or i == "Q" or i == "J":
+            hand_score += 10
+        if i == "A":
+            if hand_score + 10 > 21:
+                hand_score += 1
+            else:
+                hand_score += 10
+    player.score = hand_score
